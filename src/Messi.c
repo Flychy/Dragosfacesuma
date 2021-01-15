4c2f686d69c448f4dbbf49660c36a9482471cf53
Flychy <dragos.simion@stud.acs.upb.com>
2021-01-15 18:26:52 +0200

    Din motive necunoscute mie, Messi era gol :D

---
+++ src/Messi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int p = atoi(argv[1]);
+    int q = atoi(argv[2]);
+    int suma = p + q;
+    printf("%d", suma);
+    return 0;
+}
